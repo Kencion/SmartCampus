6b01cd954c9b92e7a7c15adff74cc9ccd6252e89
Jeforuser <1093831001@qq.com>
2017-11-22 17:24:32 +0800

    完善版

--- 代码/Data/厦大软件学院/DataClean/SQL_YHJ.py
+++ 代码/Data/厦大软件学院/DataClean/SQL_YHJ.py
@@ -1,5 +1,5 @@
 '''
-Created on 2017Äê11ÔÂ22ÈÕ
+Created on 2017ï¿½ï¿½11ï¿½ï¿½22ï¿½ï¿½
 
 @author: LENOVO
 '''
@@ -11,20 +11,20 @@ conn=pymysql.connect(
                 host='172.16.20.5',
                 user='root',
                 password='',
-                db='Èí¼þÑ§Ôº',
+                db='ï¿½ï¿½ï¿½Ñ§Ôº',
                 charset='utf8mb4'
                 )
 try:
-    #»ñÈ¡»á»°Ö¸Õë
+    #ï¿½ï¿½È¡ï¿½á»°Ö¸ï¿½ï¿½
      with conn.cursor() as cursor:
-#Éú³ÉÖ÷¼ü£ºÑ§ºÅ+Ñ§Äê£¬student_num,student_name,grade,student_type
+#ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ñ§ï¿½ï¿½+Ñ§ï¿½ê£¬student_num,student_name,grade,student_type
         sql="select distinct student_num,student_name,grade,student_type from subsidy"
         cursor.execute(sql)
         result=cursor.fetchall()
          #print(result)
         for re in result:
             count=int(re[2])
-            if str(re[3])=='ÆÕÍ¨¸ßÐ£±¾¿ÆÑ§Éú':
+            if str(re[3])=='ï¿½ï¿½Í¨ï¿½ï¿½Ð£ï¿½ï¿½ï¿½ï¿½Ñ§ï¿½ï¿½':
                 while count<=int(re[2])+3 & count<=2017:
                     sql="insert into students(student_num,student_name,student_grade,student_type) values(%s,%s,%s,%s)"
                     cursor.execute(sql,(str(re[0])+str(count),re[1],str(re[2]),re[3]))
@@ -36,7 +36,7 @@ try:
                     count=count+1
 
 
-#score±í£ºµ¼Èë×Ö¶Î Ñ§Äê
+#scoreï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ö¶ï¿½ Ñ§ï¿½ï¿½
         sql="select DISTINCT stu_num,left(school_year, 9) from score"
         cursor.execute(sql)
         result=cursor.fetchall()
@@ -52,7 +52,7 @@ try:
         conn.commit()
 
 
-#stu_in_activitiesµ¼Èë»î¶¯³ÖÐø×ÜÊ±¼ä
+#stu_in_activitiesï¿½ï¿½ï¿½ï¿½î¶¯ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ê±ï¿½ï¿½
         sql="update students set activity_last_time=%s"
         cursor.execute(sql,double(0))
        #update students set avg_hornorary_times=0;
@@ -73,9 +73,27 @@ try:
                 #print((str)(re[1][0:4])+'/'+((str)((int)(re[1][0:4])+1)))
         conn.commit()
 
-
-
-#hornorary_titleµ¼Èë±íÕÃ¼¶±ð£¬»ñ½±´ÎÊý£¨Ã¿Ñ§Äê£©£¬»ñ½±×Ü½ð¶î
+#èŽ·å–è£èª‰ç§°å·æ¬¡æ•°ï¼Œç­‰çº§
+        sql="update students set avg_hornorary_times=0"
+        cursor.execute(sql)
+        sql="select student_num,left(grant_year,4),count(*) from hornorary_handled group by student_num,left(grant_year,4)"
+        cursor.execute(sql)
+        result=cursor.fetchall()
+        for re in result:
+            sql="update students set avg_hornorary_times=%s where student_num=%s"
+            cursor.execute(sql,(re[2],(re[0]+re[1])))
+        conn.commit()
+        
+        sql="update students set hornorary_rank=null"
+        cursor.execute(sql)
+        sql="select student_num,left(grant_year,4),grant_rank from hornorary_handled"
+        cursor.execute(sql)
+        result=cursor.fetchall()
+        for re in result:
+            sql="update students set hornorary_rank=%s where student_num=%s"
+            cursor.execute(sql,(re[2],(re[0]+re[1])))
+        conn.commit()
+#hornorary_titleï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ã¼ï¿½ï¿½ð£¬»ñ½±´ï¿½ï¿½ï¿½ï¿½ï¿½Ã¿Ñ§ï¿½ê£©ï¿½ï¿½ï¿½ï¿½ï¿½Ü½ï¿½ï¿½
 #             sql="select student_num,sum(amount) as amount,grant_year,count(*) as times from hornorary_title where title_name!="" group by student_num,grant_year"
 #             cursor.execute(sql)
 #             result=cursor.fetchall()
@@ -89,7 +107,7 @@ try:
 #             conn.commit()
 
 
-#library_borrowµ¼Èëborrow_timesÃ¿Ñ§Äê½èÔÄ×Ü´ÎÊý
+#library_borrowï¿½ï¿½ï¿½ï¿½borrow_timesÃ¿Ñ§ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ü´ï¿½ï¿½ï¿½
         sql="select student_num,DATE_FORMAT(borrow_date, '%Y-%m'),count(*) from library_borrow group by student_num,DATE_FORMAT(borrow_date, '%Y-%m')"
         cursor.execute(sql)
         result=cursor.fetchall()
