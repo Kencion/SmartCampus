f819a9d322ccc078ebff7d26d72b1489c2c31380
Chihen <956795145@qq.com>
2017-11-24 12:56:18 +0800

    2017.11.24

--- 代码/FeatureCalculaters/score/GPA1.py
+++ 代码/FeatureCalculaters/score/GPA1.py
@@ -14,40 +14,49 @@ class GPA1(FeatureCalculater.FeatureCalculater):
         pass
     
     def calculate(self):
-        sql = "select student_num from students"
+        sql = "select distinct(stu_num),grade from score"
         self.executer.execute(sql)
-        print(sql)
         e = self.executer.fetchall()
         for i in e:
-            GPA1 = 0
-            credit1 = 0
-            GPA2 = 0
-            credit2 = 0
-            stu_num = str(i[0])[:-4]
-            school_year = str(i[0])[-4:]
-            next_year = str(int(school_year) + 1)
-            year1 = school_year + "/" + next_year + "-1"
-            year2 = school_year + "/" + next_year + "-2"
-            sql = "select GPA,course_credit from score where stu_num = '" + stu_num + "' and school_year = '" + year1 + "'"
-            print(sql)
-            self.executer.execute(sql)
-            stu1 = self.executer.fetchone()
-            print(stu1)
-            if(stu1 != None):
-                GPA1 = float(stu1[0])
-                credit1 = int(stu1[1])
-            sql = "select GPA,course_credit from score where stu_num = '" + stu_num + "' and school_year = '" + year2 + "'"
-            self.executer.execute(sql)
-            stu2 = self.executer.fetchone()
-            if(stu2 != None):
-                GPA2 = float(stu2[0])
-                credit2 = int(stu2[1])
-            if((credit1 + credit2) != 0):
-                GPA = (GPA1 * credit1 + GPA2 * credit2) / (credit1 + credit2)
-            print(GPA)
-            sql = "update students set GPA = " + str(GPA) + " where student_num = '" + stu_num + school_year + "'"
-            self.executer.execute(sql)
+        #     print(str(i[0]))
+            stu_num = str(i[0])
+            grade = int(i[1])
+            for year in range(grade,2017):
+                GPA1 = 0
+                credit1 = 0
+                GPA2 = 0
+                credit2 = 0
+                GPA = 0
+                year1 = str(year)+'/'+str(year+1)+'-1'
+                year2 = str(year)+'/'+str(year+1)+'-2'
+                sql = "select GPA,course_credit from score where stu_num = '"+stu_num+"' and school_year = '"+year1+"'"   
+        #         print(sql)
+                self.executer.execute(sql)
+                stu1 = self.executer.fetchone()
+        #         print(stu1)
+                
+                if stu1 is not None and stu1[0] is not None:
+                    GPA1 = float(stu1[0])
+                    credit1 = int(stu1[1])
+                sql = "select GPA,course_credit from score where stu_num = '"+stu_num+"' and school_year = '"+year2+"'"  
+        #         print(sql)
+                self.executer.execute(sql)
+                stu2 = self.executer.fetchone()
+        #         print(stu2)
+                if stu2 is not None and stu2[0] is not None:
+                    GPA2 = float(stu2[0])
+                    credit2 = int(stu2[1])
+                if((credit1+credit2)!=0):
+                    GPA = (GPA1*credit1+GPA2*credit2)/(credit1+credit2)
+                    sql = "update students set GPA = "+str(GPA)+" where student_num = '"+stu_num+str(year)+"'"
+                    self.executer.execute(sql)
+#                 else:
+#                     print("err!")
+#                     print(stu_num)
+#         print("ok")
             
     @MyLogger.myException
     def rankit(self):
         pass
+gpa = GPA1()
+gpa.calculate()
--- 代码/FeatureCalculaters/score/score1.py
+++ 代码/FeatureCalculaters/score/score1.py
@@ -6,48 +6,49 @@ from Tools import *
 from FeatureCalculaters import FeatureCalculater
  
 class score1(FeatureCalculater.FeatureCalculater):
+    
     def calculate(self):
         #yzh
-        sql = "select student_num from students"
+        sql = "select distinct(stu_num),grade from score"
         self.executer.execute(sql)
         e = self.executer.fetchall()
         for i in e:
-            score1 = 0
-            credit1 = 0
-            score2 = 0
-            credit2 = 0
-            score = 0
-            #print(str(i[0]))
-            stu_num = str(i[0])[:-4]
-            school_year = str(i[0])[-4:]
-            next_year = str(int(school_year)+1)
-            year1 = school_year + "/" +next_year+"-1"
-            year2 = school_year + "/" +next_year+"-2"
-            
-            sql = "select score,course_credit from score where stu_num = '"+stu_num+"' and school_year = '"+year1+"'"   
+        #     print(str(i[0]))
+            stu_num = str(i[0])
+            grade = int(i[1])
+            for year in range(grade,2017):
+                score1 = 0
+                credit1 = 0
+                score2 = 0
+                credit2 = 0
+                score = 0
+                year1 = str(year)+'/'+str(year+1)+'-1'
+                year2 = str(year)+'/'+str(year+1)+'-2'
+                sql = "select score,course_credit from score where stu_num = '"+stu_num+"' and school_year = '"+year1+"'"   
+        #         print(sql)
+                self.executer.execute(sql)
+                stu1 = self.executer.fetchone()
+        #         print(stu1)
+                
+                if stu1 is not None and stu1[0] is not None:
+                    score1 = float(stu1[0])
+                    credit1 = int(stu1[1])
+                sql = "select score,course_credit from score where stu_num = '"+stu_num+"' and school_year = '"+year2+"'"  
+        #         print(sql)
+                self.executer.execute(sql)
+                stu2 = self.executer.fetchone()
+        #         print(stu2)
+                if stu2 is not None and stu2[0] is not None:
+                    score2 = float(stu2[0])
+                    credit2 = int(stu2[1])
+                if((credit1+credit2)!=0):
+                    score = (score1*credit1+score2*credit2)/(credit1+credit2)
+                    sql = "update students set score = "+str(score)+" where student_num = '"+stu_num+str(year)+"'"
+                    self.executer.execute(sql)
+#                 else:
+#                     print("err!")
+#                     print(stu_num)
         
-            #print(sql)
-            self.executer.execute(sql)
-            stu1 = self.executer.fetchone()
-            #print(stu1)
-            if(stu1!=None):
-                #!!!!!!! something wrong
-                score1 = float(stu1[0])
-                credit1 = int(stu1[1])
-            
-            sql = "select score,course_credit from score where stu_num = '"+stu_num+"' and school_year = '"+year2+"'"
-            
-            self.executer.execute(sql)
-            stu2 = self.executer.fetchone()
-            if(stu2!=None):
-                score2 = float(stu2[0])
-                credit2 = int(stu2[1])
-            if((credit1+credit2)!=0):
-                score = (score1*credit1+score2*credit2)/(credit1+credit2)
-            #print(score)
-            sql = "update students set score = "+str(score)+" where student_num = '"+stu_num+school_year+"'"
-            #print(sql)
-            self.executer.execute(sql)
         #yhj
         #score表：导入字段 学年
 #         sql = "select DISTINCT stu_num,left(school_year, 9) from score"
@@ -62,3 +63,6 @@ class score1(FeatureCalculater.FeatureCalculater):
             # print(str(re[0])+re[1][0:4])
             # cursor.execute(sql)
         # sql="update students set activity_last_time=%s where student_num=%s and school_year=%s"
+    @MyLogger.myException
+    def rankit(self):
+        pass
