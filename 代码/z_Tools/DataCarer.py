f4fb1f930e4f64903b099ef1b69105bcc18ab5fe
郑龙天 <zhenglongtian@outlook.com>
2017-12-01 16:10:36 +0800

    添加了注释

--- 代码/z_Tools/DataCarer.py
+++ 代码/z_Tools/DataCarer.py
@@ -8,8 +8,6 @@ Jack:有点乱，建议修改一下
 from b_SampleProcessing.Student  import Student
 from z_Tools import MyDataBase
 from numpy import mat
-from array import array
-from test.test_audioop import datas
 
 class DataCarer():
     
@@ -18,7 +16,7 @@ class DataCarer():
     
     def createTrainDataSet(self):
         '''
-                获取训练数据
+                        获取训练数据
         @params 
         @return numpy.mat X_train:特征,numpy.mat Y_train:标签
         '''
@@ -29,7 +27,7 @@ class DataCarer():
         db = MyDataBase.MyDataBase("软件学院")
         executer = db.getExcuter()
          
-        # 先获得数据库表中的全部学生的数据
+        """先获得数据库表中的全部学生的数据"""
         executer.execute("select * from students_rank")
         dataSet = []
         for i in executer.fetchall():
@@ -38,23 +36,22 @@ class DataCarer():
         executer.close()
         dataSet = np.array(dataSet)
          
-        # 获得一些新的数据
+        """获得一些新的数据"""
         a = np.array([[]])  # 没有用的数据，单纯生成对象参数
         dip = Data_Imbalance_Processing(a, N=100)
         lists, proportion = dip._get_proportion('score')  # 分类属性
         new_dataSet = np.array(list(dip._get_data(lists, proportion, 'score')))
         
-        # 把他们加在一起以平衡数据
+        """把他们加在一起以平衡数据"""
         try:
             dataSet = np.vstack((dataSet, new_dataSet))
+            dataSet = mat(dataSet)
         except:
             pass
          
-        # 分层抽样
-        dataSet = mat(dataSet)
+        """ 对刚才的数据进行分层抽样"""
         X_train, Y_train = mat(dataSet[:, :-1]), mat(dataSet[:, -1])
         X_train=tuple(X_train.tolist())
-        
         t=list()
         for i in range(len(Y_train)):
             t.append(Y_train[i,0])
@@ -64,27 +61,28 @@ class DataCarer():
         dataSet=mat(dataSet)
  
         X_train, Y_train = mat(dataSet[:, :-1]), mat(dataSet[:, -1])
+        
         return X_train, Y_train
        
     def createValidateDataSet(self, column='score', year='2016'):
         '''
-                获取测试数据
-        @params 
+                        获取测试数据
+        @params str column:设置把那一列当成结果来预测,str year:学年
         @return list[Student] students:学生列表,numpy.mat X_test:特征
         '''
         db = MyDataBase.MyDataBase("软件学院")
         executer = db.getExcuter()
          
-        # get all the students
+        """获得所有学生的数据"""
         sql = "select * from students_rank where {0} =0 and right(student_num,4) in('{1}','2017')"
         executer.execute(sql.format(column, year))
-        students, dataSet = [], []
+        students, X_test = [], []
         for i in executer.fetchall():
             student = Student(student_num=i[0], features=list(i[1:-1]), label=i[-1])
-            dataSet.append(student.features)
+            X_test.append(student.features)
             students.append(student)
-             
         executer.close()
          
-        dataSet = mat(dataSet)
-        return students, dataSet
+        X_test = mat(X_test)
+        
+        return students, X_test
