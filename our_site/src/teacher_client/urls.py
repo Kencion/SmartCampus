58c1778d88c339d584b7616d1b8f11a6fba826b4
郑龙天 <zhenglongtian@outlook.com>
2017-12-12 22:20:41 +0800

    完善APP教师端

--- our_site/src/teacher_client/urls.py
+++ our_site/src/teacher_client/urls.py
@@ -4,10 +4,4 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^class_failing_warning$', views.class_failing_warning, name='class_failing_warning'),
-    url(r'^r$', views.r, name='r'),  # 什么意思
-    url(r'^show_infos$', views.show_infos, name='show_infos'),
-    url(r'^Single_student$', views.Single_student, name='Single_student'),
-    url(r'^show_student$', views.show_student, name='show_student'),
-    url(r'^bingzhuang_fig$', views.bingzhuang_fig, name='bingzhuang_fig'),
-    url(r'^zhexian_fig$', views.zhexian_fig, name='zhexian_fig'),
 ]
--- our_site/src/teacher_client/views.py
+++ our_site/src/teacher_client/views.py
@@ -1,13 +1,8 @@
-#coding:UTF-8
 from django.shortcuts import loader
 from django.http import HttpResponse
 import matplotlib.pyplot as plt
 import numpy as np
 import os, sys
-from django.shortcuts import render
-from PIL import Image
-import threading
-from multiprocessing.sharedctypes import template
 # Create your views here.
 
 def index(request):
@@ -23,22 +18,29 @@ def class_failing_warning(request):
     @author: Jack
     @return: 模块页面
     """
-    template = loader.get_template('teacher_client/index_old.html')
-    return HttpResponse(template.render(None, request))
+#     broken_line_chart()
+#     pie_chart()
+    
+    template = loader.get_template('teacher_client/class_failing_warning.html')
+    context = {
+        'module_name':'挂科预警',
+        }
+    return HttpResponse(template.render(context, request))
 
-def zhexian_fig(request):
+def broken_line_chart():
     """
-    @author: 
+            名字可以
+    @author:
+    @change: jack把这个函数的名字由zhexian_fig改成了 broken_line_chart
     @return: 填一下
     """
-    template=loader.get_template('teacher_client/zhexian_fig.html')
     from background_program.y_Modules.ClassFailingWarning.ClassFailingWarning import ClassFailingWarning
     t = ClassFailingWarning()
     infos = t.doit()
     def autolabel(rects):
         for rect in rects:
             height = rect.get_height()
-            plt.text(rect.get_x()+rect.get_width()/4., 1.01*height, "%s" % float(height))
+            plt.text(rect.get_x() + rect.get_width() / 4., 1.01 * height, "%s" % float(height))
     num = np.zeros(5)
     score = [x[1] for x in infos]
     
@@ -47,147 +49,43 @@ def zhexian_fig(request):
     fig = plt.figure('By SmartCampus Team')
     ax = fig.add_subplot(111)
     ax.set_title('Bar Chart')
-    colors = ['red','yellowgreen','lightskyblue','g','b']
-    labels = [u"0类",u"1类",u"2类",u"3类",u"4类"]
+    colors = ['red', 'yellowgreen', 'lightskyblue', 'g', 'b']
+    labels = [u"0类", u"1类", u"2类", u"3类", u"4类"]
     rec = plt.bar(labels, num, color=colors) 
     autolabel(rec)
     ax.set_xlabel('Student type')
     ax.set_ylabel('Student number')
-    save_path = sys.path[0]+'/teacher_client/static/teacher_client/images/zhexian.png'
+    save_path = sys.path[0] + '/teacher_client/static/teacher_client/images/broken_line_chart.png'
     plt.savefig(save_path)
     plt.close()
-    context = {
-        'infos': infos,
-    }
-    return HttpResponse(template.render(context, request))
 
-def bingzhuang_fig(request):
+def pie_chart():
     """
     @author: 
+    @change: jack把这个函数的名字由bingzhuang_fig改成了 pie_chart
     @return: 填一下
     """
-    template=loader.get_template('teacher_client/bingzhuang_fig.html')
     from background_program.y_Modules.ClassFailingWarning.ClassFailingWarning import ClassFailingWarning
     t = ClassFailingWarning()
     infos = t.doit()
     num = np.zeros(5)
-    colors = ['red','yellowgreen','lightskyblue','g','b']
-    labels = [u"0类",u"1类",u"2类",u"3类",u"4类"]
+    colors = ['red', 'yellowgreen', 'lightskyblue', 'g', 'b']
+    labels = [u"0类", u"1类", u"2类", u"3类", u"4类"]
     score = [x[1] for x in infos]
     for i in range(5):
         num[i] = score.count(i)
     fig = plt.figure('By SmartCampus Team')
     ax = fig.add_subplot(111)
     ax.set_title('Pie Chart')
-    patches,l_text,p_text = plt.pie(num,labels = labels,colors=colors,labeldistance = 1.1,autopct = '%3.1f%%',shadow = False,startangle = 90,pctdistance = 0.6) 
-    save_path = sys.path[0]+'/teacher_client/static/teacher_client/images/bingzhuang.png'
+    patches, l_text, p_text = plt.pie(num, labels=labels, colors=colors, labeldistance=1.1, autopct='%3.1f%%', shadow=False, startangle=90, pctdistance=0.6) 
+    save_path = sys.path[0] + '/teacher_client/static/teacher_client/images/pie_chart.png'
     for t in l_text:
-        t.set_size=(30)
+        t.set_size = (30)
     for t in p_text:
-        t.set_size=(20)
+        t.set_size = (20)
 # 设置x，y轴刻度一致，这样饼图才能是圆的
     plt.axis('equal')
     plt.legend()
     plt.savefig(save_path)
 #     plt.show()
     plt.close()
-    context = {
-        'infos': infos,
-    }
-    return HttpResponse(template.render(context, request))
-
-def Single_student(request):
-    """
-    @author: 
-    @return: 填一下
-    """
-    template = loader.get_template('teacher_client/input.html')
-    context = {
-        'title': "hello, my dear student, please input your student_num and school_year: ",
-    }
-    return HttpResponse(template.render(context, request))
-
-def show_student(request):
-    """
-    @author: 
-    @return: 填一下
-    """
-    from background_program.z_Tools.MyDataBase import MyDataBase
-    
-    student_num = request.POST['student_num']
-    year=request.POST['year']
-    student_num=student_num+year
-    db = MyDataBase("软件学院")
-    executer = db.getExcuter()
-    sql = "select student_num,student_name,score_rank,score from students where student_num='{0}'".format(student_num)
-    executer.execute(sql)
-    student = executer.fetchone()
-    db.close 
-    #print(student)
-    if student is None:
-        template = loader.get_template('teacher_client/input.html')
-        context={
-            'title':"hello, my dear student, please input your student_num and school_year: ",
-            'result':"Sorry,I can't find the student_num or shool_year.",
-            }
-        return HttpResponse(template.render(context, request))
-    else:
-        template = loader.get_template('teacher_client/show_student.html')
-        context = {
-            'student_num':student[0],
-            'student_name':student[1],
-            'score_rank':student[2],
-            'score':student[3],
-        }
-        return HttpResponse(template.render(context, request))
-    
-def show_infos(request):
-    """
-    @author: 
-    @return: 填一下
-    """
-    from background_program.y_Modules.ClassFailingWarning.ClassFailingWarning import ClassFailingWarning
-    
-    t = ClassFailingWarning()
-    #print(type(t))
-    infos = t.doit()
-    template = loader.get_template('teacher_client/show_infos.html')
-    context = {
-        'infos': infos,
-    }
-    return HttpResponse(template.render(context, request))
-
-def r(request):
-    """
-    @author: 
-    @return: 填一下
-    """
-    UserName= request.POST['UserName']
-    password= request.POST['password']
-    Title_category= request.POST['Title_category'] 
-    if request.POST['UserName']=='':
-        template = loader.get_template('teacher_client/Login.html')
-        context = {
-        'result': "用户名不能为空",
-        }
-        return HttpResponse(template.render(context, request))
-    elif password=='':
-        template = loader.get_template('teacher_client/Login.html')
-        context = {
-        'result': "密码不能为空",
-        }
-        return HttpResponse(template.render(context, request))    
-    if Title_category=="student":        
-        template = loader.get_template('teacher_client/input.html')
-        context = {
-            "UserName":UserName,
-            "password":password,
-            }
-        return HttpResponse(template.render(context, request))
-    else:
-        template = loader.get_template('teacher_client/index.html')
-        context = {
-            "UserName":UserName,
-            "password":password,
-            }
-        return HttpResponse(template.render(context, request))
