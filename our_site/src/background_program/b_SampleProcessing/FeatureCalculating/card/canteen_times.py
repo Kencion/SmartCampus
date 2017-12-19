329fbfbcf2d3bb9b546df5dea385635818e7ba12
Chihen <956795145@qq.com>
2017-12-19 17:31:39 +0800

    增加了几个特征的计算

--- our_site/src/background_program/b_SampleProcessing/FeatureCalculating/card/canteen_times.py
+++ our_site/src/background_program/b_SampleProcessing/FeatureCalculating/card/canteen_times.py
@@ -6,14 +6,14 @@ Created on 2017年12月19日
 from background_program.z_Tools import MyLogger
 from background_program.b_SampleProcessing.FeatureCalculating.FeatureCalculater import FeatureCalculater
  
-class Consumption(FeatureCalculater):
+class canteen_times(FeatureCalculater):
     
     @MyLogger.myException
     def calculate(self):
         '''
-                            计算总消费额
+                            计算食堂消费次数
         '''
-        print("start")
+#         print("start")
         sql = "select distinct(student_num) from card"
         self.executer.execute(sql)
         e = self.executer.fetchall()
@@ -21,12 +21,12 @@ class Consumption(FeatureCalculater):
             stu_num = str(i[0])
             for year1 in range(2014, 2017):
                 year2 = year1+1
-                sql = "select sum(transaction_amount) from card where student_num = '" + str(stu_num) + "' and date between '" + str(year1) + "-09-01' and '"+ str(year2) +"-08-31' and transaction_amount<0" 
+                sql = "select count(transaction_amount) from card where student_num = '" + str(stu_num) + "' and date between '" + str(year1) + "-09-01' and '"+ str(year2) +"-08-31' and type = 'canteen'" 
 #                 print(sql)
                 self.executer.execute(sql)
                 count = self.executer.fetchone()[0]
 #                 print(count)
-                sql = "update students set Consumption = " + str(count) + " where student_num = '" + stu_num + str(year1) + "'"
+                sql = "update students set canteen_times = " + str(count) + " where student_num = '" + stu_num + str(year1) + "'"
 #                 print(sql)
                 self.executer.execute(sql)
             print(stu_num)
@@ -34,15 +34,15 @@ class Consumption(FeatureCalculater):
         
     @MyLogger.myException
     def cluster(self):
-        maxx,minn,cent=FeatureCalculater.cluster(self,featureName='Consumption', clusters=4, sql="SELECT Consumption FROM students WHERE Consumption is not NULL")
+        maxx,minn,cent=FeatureCalculater.cluster(self,featureName='canteen_times', clusters=4, sql="SELECT canteen_times FROM students WHERE canteen_times is not NULL")
         maxx[len(maxx) - 1] = 100
         
         with open(r"Cluster_Feature", "a", encoding='utf8') as f:
-            f.write( "Consumption字段" + '\n')
+            f.write( "canteen_times字段" + '\n')
             f.write(str(0) + ':' + str(0) + ' ' + str(0) + ' ' + str(minn[0]) + '\n')  # 手动加入第一区间
             print("write.....")
             for i in range(len(cent)):
                 f.write(str(i + 1) + ':' + str(cent[i]) + ' ' + str(minn[i]) + ' ' + str(maxx[i]) + '\n')
             f.close()
-times = Consumption()
+times = canteen_times()
 times.calculate()
