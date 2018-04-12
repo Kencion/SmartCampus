ea00f896ec4f0ff787cae0fe70a3957c46f6e6e0
Chihen <956795145@qq.com>
2018-04-12 18:13:40 +0800

    之前统计少算了一学年

--- our_site/src/background_program/calculate.py
+++ our_site/src/background_program/calculate.py
@@ -52,13 +52,13 @@ calculaters = [
 #         hornorary_times.hornorary_times(),
 #            
 #         canteen_times.canteen_times(),
-#         Consumption.Consumption(),
+        Consumption.Consumption(),
 #         max_min_month_consume.max_min_month_consume(),
 #         max_every_type.max_every_type(),
 #         mean_median_var.mean_median_var(),
 #         total_amount_every_type.total_amount_every_type(),
-#         transaction_times.transaction_times(),
-#         canteen_consumption_divide_by_consumption.canteen_consumption_divide_by_consumption(),
+        transaction_times.transaction_times(),
+        canteen_consumption_divide_by_consumption.canteen_consumption_divide_by_consumption(),
         
     ]
 
