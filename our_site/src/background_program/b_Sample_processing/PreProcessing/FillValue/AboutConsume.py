2538f4fa67a73e36e0db553c937d4ffe79b33613
GCLe <842017615@qq.com>
2018-04-12 20:59:00 +0800

    。

--- our_site/src/background_program/b_Sample_processing/PreProcessing/FillValue/AboutConsume.py
+++ our_site/src/background_program/b_Sample_processing/PreProcessing/FillValue/AboutConsume.py
@@ -6,13 +6,13 @@ Created on 2018年4月11日
 from background_program.b_Sample_processing.Feature_calculating import FeatureCalculater
 def update_dataSet():
         '''
-        更新表‘students2’与钱有关的部分列
+        更新表‘students_final’与钱有关的部分列
         将null更新成0
        
         '''
-        """确定label是哪一列，并将其作为待预测对象"""
+        
         dataset =FeatureCalculater.FeatureCalculater()
-        dataset.executer.execute("DESCRIBE students2")
+        dataset.executer.execute("DESCRIBE students_final")
         columnName = dataset.executer.fetchall()
         index = -1
         for i in range(len(columnName)):
@@ -23,7 +23,7 @@ def update_dataSet():
             print('异常：未发现' + 'canteen_total_amount')
         
         for  i in range(index,len(columnName)):
-            sql = 'update students2 set  {0}=0 where {1} is null'
+            sql = 'update students_final set  {0}=0 where {1} is null'
             print(sql.format(columnName[i][0], columnName[i][0]))
             dataset.executer.execute(sql.format(columnName[i][0], columnName[i][0]))
 update_dataSet()      
